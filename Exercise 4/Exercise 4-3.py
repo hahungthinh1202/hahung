c67925757865289b9476a16b8408820fee8e5050
hahungthinh1202 <hahungthinh94@gmail.com>
2024-08-29 09:48:08 +0300

    Upload by Thinh 29/08

--- Exercise 4/Exercise 4-3.py
+++ Exercise 4/Exercise 4-3.py
@@ -2,20 +2,20 @@
 i=1
 while True:
     number_string = input("Enter a number: ")
-    if i == 1:
+    if i == 1:  #initial state
         if number_string != "":
             number_float = float(number_string)
             largest_number = smallest_number = number_float
             i = 2
-        else:                  #No input state
+        else:   #No input state
             print("No number entered")
             break
     elif i == 2:
-        if number_string == "":
+        if number_string == "": #next state exit
             print("largest number", largest_number)
             print("smallest number", smallest_number)
             break
-        else:
+        else:   #next state
             number_float = float(number_string)
             if number_float > largest_number:
                 largest_number = number_float
