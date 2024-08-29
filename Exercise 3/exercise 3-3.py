3bde54f3bc6440e499ad13231e0a6b512461a14e
hahungthinh1202 <hahungthinh94@gmail.com>
2024-08-29 09:33:19 +0300

    Upload by Thinh 29/08

--- Exercise 3/exercise 3-3.py
+++ Exercise 3/exercise 3-3.py
@@ -1,8 +1,8 @@
 #main
-gender  = input("Enter your gender: ")
+gender  = input("Enter your gender(M/F): ")
 h_value = int(input("Enter your hemoglobin value (g/l): "))
-female_list = ["Female", "female","girl","Girl", "woman", "Woman"]
-male_list   = ["male", "Male","boy","Boy", "man", "Man"]
+female_list = ["Female", "female","girl","Girl", "woman", "Woman","F"]
+male_list   = ["male", "Male","boy","Boy", "man", "Man","M"]
 if gender in female_list:
     if h_value < 117:
         print("your hemoglobin value (g/l) is too low")
@@ -17,3 +17,5 @@ elif gender in male_list:
         print("your hemoglobin value (g/l) is too high")
     else:
         print("your hemoglobin value (g/l) is normal")
+else:
+    print("invalid input")
