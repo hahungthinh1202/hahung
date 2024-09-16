2a76e7e21215f81d28b59282a5c74ff3a47c0d4e
hahungthinh1202 <hahungthinh94@gmail.com>
2024-09-16 14:24:49 +0300

    Upload by Thinh 6/09

--- Exercise 7/Exercise 7-1.py
+++ Exercise 7/Exercise 7-1.py
@@ -1,4 +1,4 @@
 #main
-season = ("spring","summer","fall","winter")
+season = "spring","summer","fall","winter"
 month = int(input("Enter the your month in number: "))
-print("The season of your month is", season[int(month/3)-1])
+print("The season of your month is", season[month//3-1])
