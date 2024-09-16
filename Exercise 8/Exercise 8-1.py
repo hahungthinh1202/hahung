6468b24efaab2eba4bcd60a2deeba6f06cb3b424
hahungthinh1202 <hahungthinh94@gmail.com>
2024-09-16 14:24:55 +0300

    Upload by Thinh 6/09

--- Exercise 8/Exercise 8-1.py
+++ Exercise 8/Exercise 8-1.py
@@ -1,19 +1,22 @@
-import mysql.connector as mysql
+import mysql.connector
 import requests
 import json
 
 #main
-connection = mysql.connect(
+connection = mysql.connector.connect(
     host='localhost',
     user='root',
     passwd='1202',
     database='flight_game'
 )
-cursor = connection.cursor()
+
 ICAO_code = input("Please enter your ICAO code: ")
-command = f"select name, municipality from airport where ident = '{ICAO_code}';"
-cursor.execute(command)
+
+cursor = connection.cursor()
+cursor.execute( f"select name, municipality from airport where ident = '{ICAO_code}';")
 data =cursor.fetchall()
+
+
 print(f"the airport with {ICAO_code} code is {data[0][0]} and located in {data[0][1]}")
 
 
--- Exercise 8/Exercise 8-2.py
+++ Exercise 8/Exercise 8-2.py
@@ -1,18 +1,19 @@
-import mysql.connector as mysql
+import mysql.connector
 
 #main
-connection = mysql.connect(
+connection = mysql.connector.connect(
     host='localhost',
     user='root',
     passwd='1202',
     database='flight_game'
 )
 
-cursor = connection.cursor()
 area_code = input("please enter the area code (example FI for Finland): ")
-command = f"select name, type from airport where iso_country = '{area_code}' order by airport.type DESC;"
-cursor.execute(command)
+
+cursor = connection.cursor()
+cursor.execute(f"select name, type from airport where iso_country = '{area_code}' order by airport.type DESC;")
 data = cursor.fetchall()
+
 print("the list of airport below")
 for row in data:
     print(f"\t{row[0]:35} {row[1]} ")
