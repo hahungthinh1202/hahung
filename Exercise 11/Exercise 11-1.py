4cb71922ebd746972175cd3ad5cdd898b039d3f3
hahungthinh1202 <hahungthinh94@gmail.com>
2024-10-22 11:47:54 +0300

    Upload by Thinh 22/10

--- Exercise 11/Exercise 11-1.py
+++ Exercise 11/Exercise 11-1.py
@@ -1,26 +1,24 @@
-class publication:
+class Publication:
     def __init__(self,name):
         self.name = name
 
-class book(publication):
+class Book(Publication):
     def __init__(self,name, author, page_count):
-        publication.__init__(self,name)
+        Publication.__init__(self,name)
         self.author = author
         self.page_count = page_count
     def print_information(self):
         print(f"Book name {self.name}, author {self.author}, page count {self.page_count}")
 
-class magazine(publication):
+class Magazine(Publication):
     def __init__(self,name,chief_editor):
-        publication.__init__(self,name)
+        Publication.__init__(self,name)
         self.chief_editor = chief_editor
     def print_information(self):
         print(f"Magazine name {self.name}, chief editor {self.chief_editor}")
 
 #main
-publication_list = []
-publication_list.append(magazine("Donald Duck","Aki Hyyppä"))
-publication_list.append(book("Compartment No. 6","Rosa Liksom", 192))
-
+publication_list = [Magazine("Donald Duck", "Aki Hyyppä"),
+                    Book("Compartment No. 6", "Rosa Liksom", 192)]
 publication_list[0].print_information()
 publication_list[1].print_information()
--- Exercise 11/Exercise 11-2.py
+++ Exercise 11/Exercise 11-2.py
@@ -1,36 +1,36 @@
-from prettytable import PrettyTable
+
 
 class Car:
-    CurSpeed = 0
+    curSpeed = 0
     TravelledDistance = 0
 
     def __init__(self, RegNumber, MaxSpeed):
         self.RegNumber = RegNumber
         self.MaxSpeed = MaxSpeed
 
-    def Accelerate(self, Acceleration):
-        self.CurSpeed += Acceleration
-        if self.CurSpeed > self.MaxSpeed:
-            self.CurSpeed = self.MaxSpeed
-        elif self.CurSpeed < 0:
-            self.CurSpeed = 0
+    def Accelerate(self, acceleration):
+        self.curSpeed += acceleration
+        if self.curSpeed > self.MaxSpeed:
+            self.curSpeed = self.MaxSpeed
+        elif self.curSpeed < 0:
+            self.curSpeed = 0
 
-    def Drive(self, Hours):
-        self.TravelledDistance = self.TravelledDistance + self.CurSpeed * Hours
+    def Drive(self, hours):
+        self.TravelledDistance = self.TravelledDistance + self.curSpeed * hours
 
 class ElectricCar(Car):
-    def __init__(self, RegNumber, MaxSpeed, BatteryCapacity):
-        Car.__init__(self, RegNumber, MaxSpeed)
-        self.BatteryCapacity = BatteryCapacity
+    def __init__(self, regNumber, maxSpeed, batteryCapacity):
+        Car.__init__(self, regNumber, maxSpeed)
+        self.BatteryCapacity = batteryCapacity
 
 class GasolineCar(Car):
-    def __init__(self, RegNumber, MaxSpeed, TankVollume):
-        Car.__init__(self, RegNumber, MaxSpeed)
-        self.TankVollume = TankVollume
+    def __init__(self, regNumber, maxSpeed, tankVollume):
+        Car.__init__(self, regNumber, maxSpeed)
+        self.TankVollume = tankVollume
 
 #main
-print("Create new car")
 
+print("Create new car")
 MyCarList = [ElectricCar("ABC-15", 180, 52.5),
              GasolineCar("ACD-123", 165, 32.3)]
 for i in range (3):
@@ -39,8 +39,8 @@ for i in range (3):
     MyCarList[0].Drive(1)
     MyCarList[1].Drive(1)
 
-print(MyCarList[0].TravelledDistance)
-print(MyCarList[1].TravelledDistance)
+print(MyCarList[0].RegNumber,"has travelled",MyCarList[0].TravelledDistance, "km")
+print(MyCarList[1].RegNumber,"has travelled",MyCarList[1].TravelledDistance, "km")
 
 
 
