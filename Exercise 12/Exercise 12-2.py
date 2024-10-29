f2cbffef1d63a16b5151ea5b8c0cee21a34369d3
hahungthinh1202 <hahungthinh94@gmail.com>
2024-10-29 12:43:47 +0200

    Upload by Thinh 29/10

--- Exercise 12/Exercise 12-2.py
+++ Exercise 12/Exercise 12-2.py
@@ -4,7 +4,7 @@ city_name = input("Please enter a place you want to know the weather: ")
 # Get the location latitude and longitude
 location = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={city_name}&limit=5&appid=dd4cee5c601907afe720b23de645a88f")
 # After request the location, it will return a list of possible place, I choose the first one in the list
-# Then using those location to make another request for the actual weather
+# Then using that location data to make another request for the actual weather
 weather = requests.get(f"https://api.openweathermap.org/data/3.0/onecall?lat={location.json()[0]['lat']}&lon={location.json()[0]['lon']}&appid=dd4cee5c601907afe720b23de645a88f")
 
 data = weather.json()
